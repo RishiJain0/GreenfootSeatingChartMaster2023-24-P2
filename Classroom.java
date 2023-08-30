8396b059f4b1968e6f04b6b094b3e7cd747b4e72
Tyler <prellwitz9986@mydusd.org>
2023-08-30 10:10:52 -0700

    tyler prellwitz addition to seating chart

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,9 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        TylerPrellwitz tylerprellwitz = new TylerPrellwitz();
+        addObject(tylerprellwitz,8,10);
+        tylerprellwitz.assignSeat();
   
     }
     
