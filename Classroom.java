52fcef176fa7f932a0aba21a54348b446f90e558
EashanMahajan <eashan2603@gmail.com>
2023-09-01 09:11:06 -0700

    Added my part of the project

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        EashanMahajan eashan = new EashanMahajan();
+        addObject(eashan, 5, 7);
+        eashan.assignSeat();
   
     }
     
