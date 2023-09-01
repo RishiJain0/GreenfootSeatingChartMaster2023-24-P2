9aebf06bc2f3e9244f94a4c40d71932896f230b7
Tanay Kotian <Kotian8395@mydusd.org>
2023-09-01 09:40:51 -0700

    Added TanayKotian seat

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,9 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        TanayKotian tanaykotian = new TanayKotian();
+        addObject(tanaykotian,5,4);
+        tanaykotian.assignSeat();
   
     }
     
