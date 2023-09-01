b098215f3a32109fe549cbaf301c8a17f3c9436a
unknown <nihalani9930@mydusd.org>
2023-09-01 09:39:42 -0700

    added in new line

--- CSALearnedSoFar.java
+++ CSALearnedSoFar.java
@@ -11,5 +11,6 @@ public interface CSALearnedSoFar
 {
     // method signatures - implement the signature below in your own class. Make sure to
     //                     match the parameter list and return type
+    public void LearnedSoFar();
     public void LearnedSoFar(String learned);
 }
