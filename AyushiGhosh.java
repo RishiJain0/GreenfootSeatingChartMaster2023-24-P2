3321fe6873006ca3f0bf38e334be4be2aa1a9694
Ayushi Ghosh <ayushighosh@Ayushis-MBP.attlocal.net>
2023-09-02 18:05:03 -0700

    Added recording for the audio along with a customized animation for the picture upon clicking of the picture (AG)

--- AyushiGhosh.java
+++ AyushiGhosh.java
@@ -66,7 +66,7 @@ public class AyushiGhosh extends Student implements SpecialInterestOrHobby
                 getName();
                 sayName(soundFile);
             
-                myHobby("I like to time travel!");
+                myHobby("I like to travel and visit places, and I love golden retrievers.");
             // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
             // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
             // Call the sitDown() method to move back  to your seat
@@ -100,26 +100,26 @@ public class AyushiGhosh extends Student implements SpecialInterestOrHobby
         setLocation(0,0);
          Greenfoot.delay(10);
         // move right
-        for (int i=1;i<=9;i++){
+        for (int i=1;i<=14;i++){
             setLocation(i,0);
             Greenfoot.delay(10);
         }
         // move back
-        for (int i=1;i<=5;i++){
-            setLocation(9,i);
+        for (int i=1;i<=10;i++){
+            setLocation(14,i);
             Greenfoot.delay(10);
         }      
          // move left
-        for (int i=9;i>=0;i--){
-            setLocation(i,5);
+        for (int i=14;i>=0;i--){
+            setLocation(i,10);
             Greenfoot.delay(10);
         }      
               // move Forward
-        for (int i=5;i>=0;i--){
+        for (int i=10;i>=0;i--){
             setLocation(0,i);
             Greenfoot.delay(10);
         }   
-           Greenfoot.delay(20);
+           Greenfoot.delay(10);
            returnToSeat();
     }
      /**
