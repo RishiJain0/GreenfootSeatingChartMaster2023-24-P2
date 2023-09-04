5daadfeb47bf04f625af909e021c9529cb004be5
JiminLim2 <lim5930@mydusd.org>
2023-09-03 20:37:28 -0700

    hi this is jimin

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        JiminLim jiminlim = new JiminLim();
+        addObject(jiminlim,6,6);
+        jiminlim.assignSeat();
   
     }
     
