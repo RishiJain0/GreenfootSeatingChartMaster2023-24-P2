e62279ac856619d8b9b2292f19e50a8367937c50
HarshaVajrala <vajrala1088@mydusd.org>
2023-09-08 09:27:19 -0700

    redid it

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        HarshaVajrala harshavajrala = new HarshaVajrala();
+        addObject(harshavajrala,9,6);
+        harshavajrala.assignSeat();
   
     }
     
